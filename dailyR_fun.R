af143c5f8b03bb8e91971cef33d45a11cb91ce1f
Jake Bowers <jake@jakebowers.org>
2021-08-31 09:41:52 -0500

    Exploration 2 is now ready

--- dailyR_fun.R
+++ dailyR_fun.R
@@ -1,4 +1,4 @@
-
+## A file to download gists and sample them for use in class
 
 library(here)
 library(gistr)
@@ -13,12 +13,22 @@ stopifnot(all(!is.na(big_sheet$days_before_today)))
 
 this_week_sheet <- filter(big_sheet,days_before_today<=7 & email!="jake@jakebowers.org")
 
-gist_auth()
-
 set.seed(12345)
 which_gists <- sample_n(this_week_sheet,2)
 dir.create(here(paste("gists-",today(),sep="")))
+
+which_gists$url[2] <- "https://gist.github.com/lcbrock2/0ce007b2d8e12bf4e7ffcba6bc53a13c"
+
 gist(which_gists$url[1]) %>% gist_save(path=here(paste("gists-",today(),sep="")))
 gist(which_gists$url[2]) %>% gist_save(path=here(paste("gists-",today(),sep="")))
 
-write.csv(which_gists,file=here(paste('which_gists-",today(),".csv",sep="")))
+
+## Liliana and Zach.
+write.csv(which_gists,file=here(paste("which_gists-",today(),".csv",sep="")))
+
+## Zach
+data("USArrests")
+mean(USArrests$Murder)
+cor(USArrests$Murder, USArrests)
+
+
